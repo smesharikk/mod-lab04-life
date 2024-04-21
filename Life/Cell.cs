6dfff9672561add4d1e50f2eafc168250849c363
smesharikk <93827477+smesharikk@users.noreply.github.com>
2024-04-21 23:31:06 +0300

    Create Cell.cs

---
+++ Life/Cell.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+using System.Threading;
+
+namespace cli_life
+{
+    public class Cell
+    {
+        public bool IsAlive;
+        public readonly List<Cell> neighbors = new List<Cell>();
+        private bool IsAliveNext;
+        public void DetermineNextLiveState()
+        {
+            int liveNeighbors = neighbors.Where(x => x.IsAlive).Count();
+            if (IsAlive)
+                IsAliveNext = liveNeighbors == 2 || liveNeighbors == 3;
+            else
+                IsAliveNext = liveNeighbors == 3;
+        }
+        public void Advance()
+        {
+            IsAlive = IsAliveNext;
+        }
+    }
+}
