2dc8c22218c26df32541213f08be96375fd0f23d
smesharikk <93827477+smesharikk@users.noreply.github.com>
2024-06-08 23:24:19 +0300

    Add files via upload

---
+++ Life/AppConfig.cs
@@ -0,0 +1,8 @@
+public class AppConfig
+{
+    public string LoadFilePath { get; set; }
+    public int BoardHeight { get; set; }
+    public int BoardWidth { get; set; }
+    public int BoardCellSize { get; set; }
+    public double BoardLiveDensity { get; set; }
+}
